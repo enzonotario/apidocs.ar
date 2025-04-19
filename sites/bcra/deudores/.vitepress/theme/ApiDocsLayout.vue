085d53e274023f366dbae9cc91e2c1bede20946e
enzonotario <enzo.notario@gmail.com>
2025-04-19 09:42:37 -0300

    chore: fixed gradient bg

--- sites/bcra/deudores/.vitepress/theme/ApiDocsLayout.vue
+++ sites/bcra/deudores/.vitepress/theme/ApiDocsLayout.vue
@@ -8,7 +8,7 @@ const { Layout } = DefaultTheme
   <Layout>
     <template #layout-top>
       <div class="relative w-full h-px">
-        <div class="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] dark:bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+        <div class="fixed top-0 z-[-2] h-screen w-screen bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] dark:bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
       </div>
     </template>
   </Layout>
