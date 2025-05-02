e8157542d51360bc7e724e6b02bdfddc87c79c93
enzonotario <enzo.notario@gmail.com>
2025-05-02 19:06:13 -0300

    chore(docs): enhance layout and styling of site cards in SitesGrid

--- docs/.vitepress/theme/components/SitesGrid.vue
+++ docs/.vitepress/theme/components/SitesGrid.vue
@@ -10,31 +10,33 @@ const latestSites = computed(() => {
 
 <template>
   <div class="py-8 space-y-6">
-    <div class="space-y-4">
+    <div class="space-y-4 text-center">
       <h1 class="!text-4xl md:!text-5xl lg:!text-6xl !font-bold text-[var(--vp-home-hero-name-color)]">
         apidocs.ar
       </h1>
       <p class="text-xl">Documentaciones de APIs públicas</p>
     </div>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="flex flex-wrap justify-center">
       <div v-for="site in latestSites"
            :key="site.subdomain"
-           class="border border-[var(--vp-c-divider)] rounded-lg p-4 hover:border-[var(--vp-c-neutral)]">
-        <h3 class="text-lg font-semibold !mt-0">
-          <a :href="`https://${site.subdomain}.apidocs.ar`"
-             target="_blank"
-             class="text-primary !no-underline hover:!underline">
-            {{ site.name }}
-          </a>
-        </h3>
-        <p class="text-[var(--vp-c-text-1)]">{{ site.description }}</p>
-        <div class="flex flex-wrap gap-2">
-          <span v-for="tag in site.tags"
-                :key="tag"
-                class="bg-[var(--vp-c-bg-alt)] text-[var(--vp-c-primary-text)] text-xs font-semibold rounded-full px-2 py-1">
-            {{ tag }}
-          </span>
+           class="w-full max-w-full sm:w-1/2 md:w-1/3 p-2 flex">
+        <div class="flex-1 border border-[var(--vp-c-divider)] rounded-lg p-4 hover:border-[var(--vp-c-neutral)]">
+          <h3 class="text-lg font-semibold !mt-0">
+            <a :href="`https://${site.subdomain}.apidocs.ar`"
+               target="_blank"
+               class="text-primary !no-underline hover:!underline">
+              {{ site.name }}
+            </a>
+          </h3>
+          <p class="text-[var(--vp-c-text-1)]">{{ site.description }}</p>
+          <div class="flex flex-wrap gap-2">
+            <span v-for="tag in site.tags"
+                  :key="tag"
+                  class="bg-[var(--vp-c-bg-alt)] text-[var(--vp-c-primary-text)] text-xs font-semibold rounded-full px-2 py-1">
+              {{ tag }}
+            </span>
+          </div>
         </div>
       </div>
     </div>
